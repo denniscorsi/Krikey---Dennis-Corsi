5f09f5874904cd4a71864fbc91983fae629e1d58
Dennis Corsi <denniscorsi@gmail.com>
2024-04-14 19:10:47 -0400

    Part 3: Build & Deploy ... completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import MainPanel from "./components/MainPanel";
 import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <NavBar />
