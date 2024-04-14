32fe932ecd648e0617f215ef02c23f63f28c872d
Dennis Corsi <denniscorsi@gmail.com>
2024-04-14 18:31:20 -0400

    Added media queries for small screens

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,20 +5,20 @@ const NavBar = () => {
         <li>
           <img src="/Krikey-AI-Logo.png" />
         </li>
-        <li>
+        <li className="menu-option">
           How to Animate <img src="/expand.png" />
         </li>
-        <li>
+        <li className="menu-option">
           Business <img src="/expand.png" />
         </li>
-        <li>
+        <li className="menu-option">
           Education <img src="/expand.png" />
         </li>
-        <li>
+        <li className="menu-option">
           Social Media <img src="/expand.png" />
         </li>
-        <li>Pricing</li>
-        <li>About Us</li>
+        <li className="menu-option">Pricing</li>
+        <li className="menu-option">About Us</li>
       </ul>
       <button>Get Started</button>
     </div>
